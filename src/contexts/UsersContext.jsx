877708efc3f0087bebef2ef2d0e1afed8bf38e62
Efiamotu-1 <musahabeebefy10@gmail.com>
2024-02-18 17:14:24 +0000

    clean code

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -23,7 +23,6 @@ function UserProvider({children}) {
         try {
           const response = await fetch(`https://jsonplaceholder.typicode.com/comments?_start=${page}&_limit=10`);
           const data = await response.json();
-          console.log(data)
           if (data.length === 0) {
             setHasMore(false);
           } else {
